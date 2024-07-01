d615f2bb6603e7ce7c21ae1c5a7f91edb8290580
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 12:10:30 +0900

    url endpoints for each view

--- Backend/s3_handler/urls.py
+++ Backend/s3_handler/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-app_name = "s3_handler"
+from .views import (
+  S3ImageUploadEvent,
+  S3VideoUploadEvent,
+  S3AudioUploadEvent,
+  S3FileUploadEvent,
+)
+
+app_name = 's3_handler'
 
 urlpatterns = [
-  
+  path("upload/image/", S3ImageUploadEvent.as_view(), name='image_upload'),
+  path("upload/video/", S3VideoUploadEvent.as_view(), name='video_upload'),
+  path("upload/audio/", S3AudioUploadEvent.as_view(), name='audio_upload'),
+  path("upload/file/", S3FileUploadEvent.as_view(), name='file_upload'),
 ]
