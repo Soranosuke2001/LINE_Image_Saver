d8750cf18d3e6d7ed0ade06828e08719b30a9a4a
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:45:30 +0900

    url endpoint for audio events

--- Backend/line_bot/urls.py
+++ Backend/line_bot/urls.py
@@ -4,6 +4,7 @@ from .views import (
   WebhookEvent,
   LineImageEvent,
   LineVideoEvent,
+  LineAudioEvent,
 )
 
 app_name = "line_bot"
@@ -12,4 +13,6 @@ urlpatterns = [
   path("", WebhookEvent.as_view(), name="webhook_handler"),
   path("submit/image/", LineImageEvent.as_view(), name="image_handler"),
   path("submit/video/", LineVideoEvent.as_view(), name="video_handler"),
+  path("submit/audio/", LineAudioEvent.as_view(), name="audio_handler"),
+
 ]
