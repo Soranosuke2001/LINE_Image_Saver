1974873ea02e7fc62e6deda698107d0863f41d5e
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:19:20 +0900

    line video class

--- Backend/line_bot/models.py
+++ Backend/line_bot/models.py
@@ -17,4 +17,24 @@ class LineImage(models.Model):
   def __str__(self):
     return self.image_url
   
+
+class LineVideo(models.Model):
+  video_url = models.CharField(max_length=256)
+  preview_image_url = models.CharField(max_length=256, blank=True)
+  duration_ms = models.IntegerField()
+
+  content_provider = models.CharField(max_length=10)
+  source_type = models.CharField(max_length=10)
+  reply_token = models.CharField(max_length=100)
+  is_redelivery = models.BooleanField()
+
+  user_id = models.CharField(max_length=256)
+  webhook_event_id = models.CharField(max_length=256)
+
+  timestamp = models.DateTimeField()
+  received_date = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    return self.video_url
+  
   
