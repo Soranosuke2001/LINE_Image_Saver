7cca308548bd19e8f857f8f4dc3fe83119e36787
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:14:00 +0900

    created url endpoint for image post event

--- Backend/line_bot/urls.py
+++ Backend/line_bot/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
   WebhookEvent,
+  LineImageEvent,
 )
 
 app_name = "line_bot"
 
 urlpatterns = [
   path("", WebhookEvent.as_view(), name="webhook_handler"),
+  path("submit/image/", LineImageEvent.as_view(), name="image_handler"),
 ]
