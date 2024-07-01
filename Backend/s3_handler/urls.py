855cf4778b902df23ce86d8394b4b62daad99b60
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 12:12:23 +0900

    url paths for each GET endpoint

--- Backend/s3_handler/urls.py
+++ Backend/s3_handler/urls.py
@@ -5,6 +5,11 @@ from .views import (
   S3VideoUploadEvent,
   S3AudioUploadEvent,
   S3FileUploadEvent,
+
+  S3ImageFetchEvent,
+  S3VideoFetchEvent,
+  S3AudioFetchEvent,
+  S3FileFetchEvent,
 )
 
 app_name = 's3_handler'
@@ -14,4 +19,9 @@ urlpatterns = [
   path("upload/video/", S3VideoUploadEvent.as_view(), name='video_upload'),
   path("upload/audio/", S3AudioUploadEvent.as_view(), name='audio_upload'),
   path("upload/file/", S3FileUploadEvent.as_view(), name='file_upload'),
+  
+  path("fetch/image/", S3ImageFetchEvent.as_view(), name='image_fetch'),
+  path("fetch/video/", S3VideoFetchEvent.as_view(), name='video_fetch'),
+  path("fetch/audio/", S3AudioFetchEvent.as_view(), name='audio_fetch'),
+  path("fetch/file/", S3FileFetchEvent.as_view(), name='file_fetch'),
 ]
