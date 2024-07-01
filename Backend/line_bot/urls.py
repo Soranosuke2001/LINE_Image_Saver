815636695fdd91a9d51a86d1fd9696d23237b5bf
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:34:11 +0900

    url endpoint to handle video events

--- Backend/line_bot/urls.py
+++ Backend/line_bot/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
   WebhookEvent,
   LineImageEvent,
+  LineVideoEvent,
 )
 
 app_name = "line_bot"
@@ -10,4 +11,5 @@ app_name = "line_bot"
 urlpatterns = [
   path("", WebhookEvent.as_view(), name="webhook_handler"),
   path("submit/image/", LineImageEvent.as_view(), name="image_handler"),
+  path("submit/video/", LineVideoEvent.as_view(), name="video_handler"),
 ]
