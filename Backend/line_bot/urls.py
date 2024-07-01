48a3413dc0883ff265c2229f1037f7833725775f
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:02:42 +0900

    created main url endpoint

--- Backend/line_bot/urls.py
+++ Backend/line_bot/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from .views import (
+  WebhookEvent,
+)
+
 app_name = "line_bot"
 
 urlpatterns = [
-  path()
+  path("", WebhookEvent.as_view(), name="webhook_handler"),
 ]
