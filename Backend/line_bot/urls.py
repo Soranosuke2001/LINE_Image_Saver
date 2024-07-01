6ec04748e29ff662a372c3052093d42521c748f0
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:51:20 +0900

    file event endpoint created

--- Backend/line_bot/urls.py
+++ Backend/line_bot/urls.py
@@ -5,6 +5,7 @@ from .views import (
   LineImageEvent,
   LineVideoEvent,
   LineAudioEvent,
+  LineFileEvent,
 )
 
 app_name = "line_bot"
@@ -14,5 +15,6 @@ urlpatterns = [
   path("submit/image/", LineImageEvent.as_view(), name="image_handler"),
   path("submit/video/", LineVideoEvent.as_view(), name="video_handler"),
   path("submit/audio/", LineAudioEvent.as_view(), name="audio_handler"),
+  path("submit/file/", LineFileEvent.as_view(), name="file_handler"),
 
 ]
