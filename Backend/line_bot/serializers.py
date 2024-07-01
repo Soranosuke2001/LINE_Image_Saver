9e6888f9239d982b64564a081409b5aa36b53a8c
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-01 11:19:49 +0900

    serlizer for video class

--- Backend/line_bot/serializers.py
+++ Backend/line_bot/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import (
   LineImage,
+  LineVideo,
 )
 
 
@@ -19,3 +20,19 @@ class LineImageSerializer(serializers.ModelSerializer):
       "timestamp",
     ]
 
+
+class LineVideoSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = LineVideo
+    fields = [
+      "video_url",
+      "preview_image_url",
+      "duration_ms",
+      "content_provider",
+      "source_type",
+      "reply_token",
+      "is_redelivery",
+      "user_id",
+      "webhook_event_id",
+      "timestamp",
+    ]    
