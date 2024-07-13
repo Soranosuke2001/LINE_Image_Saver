7f344308729445bf55aa8bbc26ad475a7d4fa350
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-13 18:30:53 +0900

    now filters by the month and year

--- Backend/line_api/s3_handler/views.py
+++ Backend/line_api/s3_handler/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.db.models.functions import ExtractMonth, ExtractYear
 
 import boto3
 
@@ -172,9 +173,21 @@ class S3FileUploadEvent(APIView):
 @method_decorator(csrf_exempt, name='dispatch')
 class S3ImageFetchEvent(APIView):
   def get(self, request, format=None):
-    # Add some verification step here
-    # ....
-    s3_images = S3LineImage.objects.all()
+    month = request.query_params.get('month')
+    year = request.query_params.get('year')
+
+    if not month or not year:
+      return Response("Invalid Request", status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+      month = int(month)
+      year = int(year)
+    except ValueError:
+      return Response("Invalid Request", status=status.HTTP_400_BAD_REQUEST)
+
+    
+      
+    s3_images = S3LineImage.objects.annotate(month=ExtractMonth('timestamp'), year=ExtractYear('timestamp')).filter(month=month, year=year).order_by('-timestamp')
 
     serializer = S3LineImageSerializer(s3_images, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
