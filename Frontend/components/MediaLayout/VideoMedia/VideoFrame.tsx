d1f93ee8c3ba19fef68a8dcb696d2dfb26a27acf
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-15 18:27:29 +0900

    development still working on styling each video card

--- Frontend/components/MediaLayout/VideoMedia/VideoFrame.tsx
+++ Frontend/components/MediaLayout/VideoMedia/VideoFrame.tsx
@@ -2,8 +2,10 @@
 
 import React, { useState } from "react";
 
-import { VideoData } from "@/lib/types";
 import VideoModal from "./VideoModal";
+import { FaCirclePlay } from "react-icons/fa6";
+
+import { VideoData } from "@/lib/types";
 
 const VideoFrame = ({ video }: { video: VideoData }) => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -21,17 +23,22 @@ const VideoFrame = ({ video }: { video: VideoData }) => {
 
   return (
     <>
-      <div
-        className="w-full"
-        onClick={() => openModal(video.video_url)}
-      >
-        {video.video_id}
+      <div className="w-full border border-solid border-black" onClick={() => openModal(video.video_url)}>
+        {video.preview_image_url === "" ? (
+          <div className="w-full h-24 flex justify-center items-center bg-black">
+            <FaCirclePlay size={30} className="text-neutral-300"/>
+          </div>
+        ) : (
+          <img src={video.preview_image_url} alt="Video preview image" />
+        )}
+        <div className="flex flex-col">
+          <span>{video.video_id}</span>
+          <span>{video.duration}</span>
+          <span>{video.timestamp.toISOString().split('T')[0]}</span>
+        </div>
       </div>
       {isModalOpen && selectedVideo && (
-        <VideoModal
-          video_url={selectedVideo}
-          onClose={closeModal}
-        />
+        <VideoModal video_url={selectedVideo} onClose={closeModal} />
       )}
     </>
   );
