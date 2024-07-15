13ac177c922deb014137718d63f7a72e0f2dc1a5
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-15 10:46:53 +0900

    google fonts

---
+++ Frontend/app/fonts.ts
@@ -0,0 +1,9 @@
+import { Josefin_Sans, Outfit } from 'next/font/google'
+
+export const josefin_sans = Josefin_Sans({
+  subsets: ['latin'],
+})
+
+export const outfit_font = Outfit({
+  subsets: ['latin']
+})
