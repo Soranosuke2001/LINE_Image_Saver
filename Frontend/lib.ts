1a6b0c6146edad5a897a89b59f08e1852a38150b
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-15 11:17:14 +0900

    jwt token sign function

---
+++ Frontend/lib.ts
@@ -0,0 +1,12 @@
+import { SignJWT } from "jose"
+
+const secretKey = process.env.ENCRYPT_SECRET_KEY!
+const key = new TextEncoder().encode(secretKey);
+
+export async function encrypt(payload: any) {
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("10 sec from now")
+    .sign(key);
+}
