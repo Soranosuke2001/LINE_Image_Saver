5d2dc556b2a8cda4fc995249a04cd403c5388cae
Soranosuke2001 <sorazora1.4@gmail.com>
2024-07-15 12:44:37 +0900

    updated routes implemented fetch media files route updated logout route

---
+++ Frontend/app/api/fetch/image/route.ts
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { monthMap } from "@/lib/constants";
+
+export async function GET() {
+  const month = cookies().get('image_month')?.value
+  const year = cookies().get('image_year')?.value
+
+  // The initial case, when the user has not seen anything
+  if (!month || !year) {
+    const response = await axios.get(process.env.FETCH_IMAGES!, { params: {
+      'initial': true
+    }})
+
+    if (response.status !== 200) {
+      return new NextResponse("Unable to fetch media files", { status: response.status })
+    }
+
+    const { media_files, month, year } = response.data
+
+    let new_month: number = +month
+    let new_year: number = +year
+
+    if (media_files.length !== 0) {
+      new_month -= 1
+
+      if (new_month === 0) {
+        new_month = 12
+        new_year -= 1
+      }
+    }
+
+    cookies().set('image_month', new_month.toString())
+    cookies().set('image_year', new_year.toString())
+
+    // @ts-ignore
+    return NextResponse.json({ message: media_files, month: monthMap[month], year }, { status: 200 })
+  }
+
+  // The other case, fetching the next month
+  const response = await axios.get(process.env.FETCH_IMAGES!, { params: {
+    month,
+    year
+  }})
+
+  if (response.status !== 200) {
+    return new NextResponse("Unable to fetch media files", { status: response.status })
+  }
+
+  let new_month: number = +month
+  let new_year: number = +year
+
+  if (response.data.length !== 0) {
+    new_month -= 1
+
+    if (new_month === 0) {
+      new_month = 12
+      new_year -= 1
+    }
+  }
+
+  cookies().set('image_month', new_month.toString())
+  cookies().set('image_year', new_year.toString())
+
+  // @ts-ignore
+  return NextResponse.json({ message: response.data, month: monthMap[month], year }, { status: 200 })
+}
--- Frontend/lib/constants.ts
+++ Frontend/lib/constants.ts
@@ -26,3 +26,18 @@ export const imageFrameProps = [
 ]
 
 export const lineLoginLink = 'https://help.line.me/official_account_jp/ios/categoryId/20008250/pc?contentId=20013136'
+
+export const monthMap = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+}
